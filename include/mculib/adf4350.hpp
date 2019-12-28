0b7bb82cacc1e8640bd6c4e65ee8aaa4d1916520
gabu-chan <OwOwOwOwO123@outlook.com>
2019-12-28 13:15:42 +0000

    adf4350: add adf4351 support; make more parameters configurable

--- include/mculib/adf4350.hpp
+++ include/mculib/adf4350.hpp
@@ -19,10 +19,26 @@ namespace ADF4350 {
 		int R = 1;
 		int rfPower = 0b01;
 		int auxPower = 0b00;
+
+		// band select divider. 1 to 255.
+		int bsDivider = 255;
+
+		// charge pump current, 0 to 15.
+		int cpCurrent = 15;
+
+		// CLKDIV divider (for fastlock and phase resync). 0 to 4095.
+		int clkDivDivider = 6;
+
+		enum {
+			CLKDIVMODE_OFF = 0b00,
+			CLKDIVMODE_FASTLOCK = 0b01,
+			CLKDIVMODE_RESYNC = 0b10
+		} clkDivMode;
+
 		bool rfEnable = true;
 		bool auxEnable = false;
 		bool feedbackFromDivided = false;
-
+		bool lowSpurMode = true;
 
 		ADF4350Driver(const sendWord_t& _sendWord): sendWord(_sendWord) {}
 
@@ -36,32 +52,35 @@ namespace ADF4350 {
 				case 4:  odiv=0b010; break;
 				case 8:  odiv=0b011; break;
 				case 16: odiv=0b100; break;
+				case 32: odiv=0b101; break;
+				case 64: odiv=0b110; break;
 				default: odiv = 0b000; break;
 			}
 			
 			int rfEn = rfEnable ? 1 : 0;
 			int auxEn = auxEnable ? 1 : 0;
 			int fb = feedbackFromDivided ? 0 : 1;
+			int noiseMode = lowSpurMode ? 0b11 : 0b00;
 
 			// reg 5
 			//        LD pin      register 5
 			sendWord((0b01<<22) | 0b101);
 
 			// reg 4
-			//        fb        rf divider    bs divider    aux en      aux pwr         rf en         rf pwr     register 4
-			sendWord((fb<<23) | (odiv<<20) 	| (255<<12) | (auxEn<<8) | (auxPower<<6) | (rfEn<<5) | (rfPower<<3) | 0b100);
+			//        fb        rf divider       bs divider       aux en      aux pwr         rf en         rf pwr     register 4
+			sendWord((fb<<23) | (odiv<<20) 	| (bsDivider<<12) | (auxEn<<8) | (auxPower<<6) | (rfEn<<5) | (rfPower<<3) | 0b100);
 			
 			// reg 3
-			//      clkdiv mode    clkdiv   register 3
-			sendWord((0b00<<15) | (80<<3) | 0b011);
+			//           clkdiv mode             clkdiv           register 3
+			sendWord((int(clkDivMode)<<15) | (clkDivDivider<<3) | 0b011);
 			
 			// reg 2
-			//   low spur mode     muxout    reference db/div2    R       CP current    int-N    LDP     PD pol   register 2
-			sendWord((0b00<<29) | (0b001<<26) | (0b00 << 24) | (R<<14) | (0b1111<<9) | (1<<8) | (0<<7) | (1<<6) | 0b010);
+			//        low spur mode     muxout    reference db/div2    R          CP current    int-N    LDP     PD pol   register 2
+			sendWord((noiseMode<<29) | (0b001<<26) | (0b00 << 24) | (R<<14) | (cpCurrent<<9) | (0<<8) | (0<<7) | (1<<6) | 0b010);
 			
 			// reg 1
 			//      prescaler   phase  frac modulus
-			sendWord((1<<27) | (1<<15) | (denominator<<3) | 0b001);
+			sendWord((1<<27) | (0<<15) | (denominator<<3) | 0b001);
 		}
 
 		// sets integer N and numerator
